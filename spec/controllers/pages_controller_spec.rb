0ce55702ca1b721d530a4d98997b08c8d4d88dbf
Joseph Haske <haskejl25@uww.edu>
2012-02-07 13:01:24 -0600

    Added chapter3=>true to pages_controller_spec.rb

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,47 +4,47 @@ describe PagesController do
   render_views
 
   describe "GET 'home'" do
-    it "should be successful" do
+    it "should be successful", :chapter3=>"true" do
       get 'home'
       response.should be_success
     end
 
-    it "should have the right title" do
+    it "should have the right title", :chapter3=>"true" do
       get 'home'
       response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
   describe "GET 'contact'" do
-    it "should be successful" do
+    it "should be successful", :chapter3=>"true" do
       get 'contact'
       response.should be_success
     end
 
-    it "should have the right title" do
+    it "should have the right title", :chapter3=>"true" do
       get 'contact'
       response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 
   describe "GET 'about'" do
-    it "should be successful" do
+    it "should be successful", :chapter3=>"true" do
       get 'about'
       response.should be_success
     end
 
-    it "should have the right title" do
+    it "should have the right title", :chapter3=>"true" do
       get 'about'
       response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | About")
     end
   end 
   describe "GET 'help'" do
-    it "should be successful" do
+    it "should be successful", :chapter3=>"true" do
       get 'help'
       response.should be_success
     end
 
-    it "should have the right title" do
+    it "should have the right title", :chapter3=>"true" do
       get 'help'
       response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Help")
     end

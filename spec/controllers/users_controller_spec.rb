a5a4940d5080cdecb5fbd485a7e301ae9fd9f203
Joseph Haske <haskejl25@uww.edu>
2012-03-08 13:36:44 -0600

    user model with passwords, and user show page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,6 +3,37 @@ require 'spec_helper'
 describe UsersController do
   render_views
 
+  describe "GET 'show'" do
+    before(:each) do
+      @user = Factory(:user)
+    end
+
+    it "should be successful", :chapter7 => "true" do
+      get :show, :id => @user
+      response.should be_success
+    end
+
+    it "should find the right user", :chapter7 => "true" do
+      get :show, :id => @user
+      assigns(:user).should == @user
+    end
+
+    it "should have the right title", :chapter7 => "true" do
+      get :show, :id => @user
+      response.should have_selector("title", :content => @user.name)
+    end
+
+    it "should include the user's name", :chapter7 => "true" do
+      get :show, :id => @user
+      response.should have_selector("h1", :content => @user.name)
+    end
+
+    it "should have a profile image", :chapter7 => "true" do
+      get :show, :id => @user
+      response.should have_selector("h1>img", :class => "gravatar")
+    end
+  end
+
   describe "GET 'new'" do
     it "should be successful", :chapter5 => "true" do
       get 'new'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,10 +14,13 @@ require 'spec_helper'
 describe User do
 
   before(:each) do
-    @attr = { :name => "Example User", :email => "user@example.com" }
+    @attr = { :name => "Example User",
+              :email => "user@example.com",
+              :password => "foobar",
+              :password_confirmation => "foobar" }
   end
 
-  it "should create a new instance given valid attributes", :chapter6 => "true" do
+  it "should create a new instance given valid attributes", :chapter6 => "true", :chapter7 => "true" do
     User.create!(@attr)
   end
 
@@ -65,5 +68,68 @@ describe User do
     User.create!(@attr.merge(:email => upcased_email))
     user_with_duplicate_email = User.new(@attr)
     user_with_duplicate_email.should_not be_valid
+  end
+
+  describe "password validations" do
+    it "should require a password", :chapter7 => "true" do
+      User.new(@attr.merge(:password => "", :password_confirmation => "")).should_not be_valid
+    end
+
+    it "should require a matching password confirmation", :chapter7 => "true" do
+      User.new(@attr.merge(:password_confirmation => "invalid")).should_not be_valid
+    end
+ 
+    it "should reject short passwords", :chapter7 => "true" do
+      short = "a" * 5
+      hash = @attr.merge(:password => short, :password_confirmation => short)
+      User.new(hash).should_not be_valid
+    end
+
+    it "should reject long passwords", :chapter7 => "true" do
+      long = "a" * 41
+      hash = @attr.merge(:password => long, :password_confirmation => long)
+      User.new(hash).should_not be_valid
+    end
+  end
+
+  describe "password encryption" do
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+
+    it "should have an encrypted password attribute", :chapter7 => "true" do
+      @user.should respond_to(:encrypted_password)
+    end
+
+    it "should set the encrypted password", :chapter7 => "true" do
+      @user.encrypted_password.should_not be_blank
+    end
+ 
+    describe "has_password? method" do
+      it "should be true if the passwords match", :chapter7 => "true" do
+        @user.has_password?(@attr[:password]).should be_true
+      end
+
+      it "should be false if the passwords don't match", :chapter7 => "true" do
+        @user.has_password?("invalid").should be_false
+      end
+    end
+
+    describe "authentication method", :chapter7 => "true" do
+      it "should return nil on email/password mismatch", :chapter7 => "true" do
+        wrong_password_user = User.authenticate(@attr[:email], "wrongpass")
+        wrong_password_user.should be_nil
+      end
+    
+      it "should return nil for an email address with no user", :chapter7 => "true" do
+        nonexistent_user = User.authenticate("bar@foo.com", @attr[:password])
+        nonexistent_user.should be_nil
+      end
+
+      it "should return the user on email/password match", :chapter7 => "true" do
+        matching_user = User.authenticate(@attr[:email], @attr[:password])
+        matching_user.should == @user
+      end
+    end
   end
 end
